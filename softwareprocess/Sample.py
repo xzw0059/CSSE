3e5648f73814fe4ed823acfdc1307d07d3b9281d
xzw0059 <xzw0059@auburn.edu>
2017-02-20 18:13:52 +0800

    2017-02-20

--- softwareprocess/Sample.py
+++ softwareprocess/Sample.py
@@ -1,38 +1,74 @@
-from unittest import TestCase
-import softwareprocess.Sample
+import math
+class Sample(object):
 
-class SampleTest(Teastcase):
-    pass
+# outward facing methods
+    def __init__(self, n=None):
+        functionName = "Sample.__init__: "
+        if(n == None):
+            raise ValueError(functionName + "invalid n")
+        if(not(isinstance(n, int))):
+            raise ValueError(functionName + "invalid n")
+        if((n < 2) or (n >= 30)):
+            raise ValueError(functionName + "invalid n")
+        self.n = n
 
-# ______________
-# ____acceptance tests
-#   100 constructor
-#   Desired level of confidence Boundary Value Analysis
-#       Input/Ourput analysis:
-#       input: n -> interger .GE. 2 and .LT 30. mandatory, unvalidated
-#       Output:instance of Sample
-#       Happy path analysis;
-#           n: nominal value  4 expected result: instance of sample
-#              low bound      2  expected result: instance of sample
-#               high bound    29 expected result: instance of sample
-#       San path analysis:
-#           n: non-int n        4.2 expected result: exception
-#           n: out of bounds n 1  expected result: exception
-#               ditto           30 expected result: ditto
-#           n: is missing       missing n       expected result: exception
-#
-#  Happy path
-    def     test100_010_sholdCoustructInstance(self);
-            self.assertIsInstance()S.Samle(4),S.Sample
-    def test100_020_ShouldConstructInstanceWithLowBound(self):
-        myS = S.Sample(2)
-        self.assertIsInstance(myS.S.Sample)
-        self.assertEauIs(myS, getN(),2)
-    def test100_020_ShouldConstructInstanceWithLowBound(self):
-        myS = S.Sample(29)
-        self.assertIsInstance(myS.S.Sample)
-        self.assertEauIs(myS, getN(),29)
-    def test100_910_ShouldConstructInstanceWithLowBound(self):
-        expectdString = "Sample.__init__: invalid n"
-        with self.asserRaises(ValueError) as context;
-            myS
+    def getN(self):
+        return self.n
+
+    
+    def p(self, t=None, tails=1):
+        functionName = "Sample.p: "
+        if(t == None):
+            raise ValueError(functionName + "missing t")
+        if(not(isinstance(t, float))):
+            raise ValueError(functionName + "invalid t")
+        if(t < 0.0):
+            raise ValueError(functionName + "invalid t")
+        
+        if(not(isinstance(tails, int))):
+            raise ValueError(functionName + "invalid tails")
+        if((tails != 1) & (tails != 2)):
+            raise ValueError(functionName + "invalid tails")
+        
+        constant = self.calculateConstant(self.n)
+        integration = self.integrate(0.0,t, self.n, self.f)
+        if(tails == 1):
+            result = constant * integration + 0.5
+        else:
+            result = constant * integration * 2
+            
+        if(result > 1.0):
+            raise ValueError(functionName + "result > 1.0")
+        
+        return result
+        
+# internal methods
+    def gamma(self, x):
+        if(x == 1):
+            return 1
+        if(x == 0.5):
+            return math.sqrt(math.pi)
+        return (x - 1) * self.gamma(x - 1)
+    
+    def calculateConstant(self, n):
+        n = float(n)
+        numerator = self.gamma((n + 1.0) / 2.0)
+        denominator = self.gamma(n / 2.0) * math.sqrt(n * math.pi)
+        result = numerator / denominator
+        return result
+    
+    def f(self, u, n):
+        n = float(n)
+        base = (1 + (u ** 2) / n)
+        exponent = -(n + 1.0) / 2
+        result = base ** exponent
+        return result
+    
+    def integrate(self, 0.0, highBound, n, f):
+        pass
+        
+        
+    
+        
+            
+        
