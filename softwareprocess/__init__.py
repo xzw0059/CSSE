f13a1da91dfead181f702b1f4a77b1b7de78a859
xzw0059 <xzw0059@auburn.edu>
2017-03-20 17:07:18 -0500

    Green Light |

--- softwareprocess/__init__.py
+++ softwareprocess/__init__.py
@@ -1,20 +1,6 @@
-def return_continue_break(type):
-    if(not type in ["return", "continue", "break"]):
-        print '"type" should be "return, continue, break".'
-        return
-    for j in range(0, 10):
-        for i in range(0, 10):
-            print "j_i: %d_%d" %(j, i)
-            if(i > 3):
-               if(type == "return"):
-                   return
-               elif(type == "continue"):
-                   continue
-               else:
-                   break
-            print "executed!"
-
-if __name__ == '__main__':
-    return_continue_break("break")
-    return_continue_break("continue")
-    return_continue_break("return")
+for i in range(10):
+    #如果是2的倍数，那么不执行接下来的动作，你改成break试一下，会有别的情况
+    if i % 2 == 0:
+        continue
+    #打印
+    print i
