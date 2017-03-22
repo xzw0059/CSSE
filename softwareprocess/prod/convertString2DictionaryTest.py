759959ad2df716cfc06ae081275a753f96c0518d
xzw0059 <xzw0059@auburn.edu>
2017-03-22 17:41:51 -0500

    Green Light |

--- softwareprocess/prod/convertString2DictionaryTest.py
+++ softwareprocess/prod/convertString2DictionaryTest.py
@@ -46,7 +46,7 @@ class convertString2DictionaryTest(TestCase):
         values={'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         result={'altitude':'45d11.9', 'observation': '45d15.2', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
         self.assertDictEqual(dispatch.dispatch(values),result)
-        print values
+        print dispatch(values)
     # def test_100_010_ShouldBeNotNone(self):
     #     values={}
     #     self.assertDictEqual(dispatch.dispatch(values), {'error':'no op  is specified'})
