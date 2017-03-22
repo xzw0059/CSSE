43cd7d5f8660e663e4122d3692daf723ad60b559
xzw0059 <xzw0059@auburn.edu>
2017-03-22 08:51:42 -0500

    Green Light |

--- softwareprocess/prod/dispatchTest.py
+++ softwareprocess/prod/dispatchTest.py
@@ -16,9 +16,12 @@ import math
 # if __name__ == '__main__':
 #     unittest.main()
 # print math.tan(90d30)
-tempalt = math.modf(123.678)
-D2 = tempalt[1]
-M2 = tempalt[0]
-print D2
-print math.modf(123.678)
-print M2
+# tempalt = math.modf(123.678)
+# D2 = tempalt[1]
+# M2 = tempalt[0]
+# print D2
+# print math.modf(123.678)
+# print M2
+x=15
+y=25
+print x+y/5
