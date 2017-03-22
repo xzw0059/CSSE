e3c71cf07cc520f0e4651dab13f1cdfd95c75d89
xzw0059 <xzw0059@auburn.edu>
2017-03-22 17:48:56 -0500

    Green Light |

--- softwareprocess/prod/dispatch.py
+++ softwareprocess/prod/dispatch.py
@@ -122,7 +122,7 @@ def dispatch(values=None,dip=None):
         # tempalt = str(altitude0).split('.')
         D2 = int(altitude0)
         M2 = (altitude0 - D2)*60
-        altitudedig = str(D2) + 'D' + str(round(M2,1))
+        altitudedig = str(D2) + 'd' + str(round(M2,1))
         print type(altitudedig)
 
 
