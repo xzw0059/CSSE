47ec1a35ec48ccd274b01f8c70d7b1bac8bb0d21
xzw0059 <xzw0059@auburn.edu>
2017-03-22 18:24:07 -0500

    Green Light |

--- softwareprocess/prod/convertString2DictionaryTest.py
+++ softwareprocess/prod/convertString2DictionaryTest.py
@@ -37,7 +37,7 @@ class convertString2DictionaryTest(TestCase):
 
         # print dip.
     def test_100_040_RightValuesPdfEx1(self):
-        values={'observation': '10d00.0', 'height': '6.0','pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '72'}
+        values={'observation': '10d00.0', 'height': '6.0','pressure': '1010', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '72'}
         result={'altitude':'9d54.7','temperature': '72', 'height': '6.0', 'pressure': '1010', 'horizon': 'artificial', 'observation': '10d00.0', 'op': 'adjust'}
         self.assertDictEqual(dispatch.dispatch(values),result)
         print dispatch.dispatch(values)
