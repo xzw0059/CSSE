6659a3f89827fb0ecd2fa5228572861c7dc660cd
xzw0059 <xzw0059@auburn.edu>
2017-03-22 17:50:55 -0500

    Green Light |

--- softwareprocess/prod/convertString2DictionaryTest.py
+++ softwareprocess/prod/convertString2DictionaryTest.py
@@ -40,13 +40,13 @@ class convertString2DictionaryTest(TestCase):
         values={'observation': '10d00.0', 'height': '6.0', 'pressure': '1010', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '72'}
         result={'altitude':'9d54.7','temperature': '72', 'height': '6.0', 'pressure': '1010', 'horizon': 'artificial', 'observation': '10d00.0', 'op': 'adjust'}
         self.assertDictEqual(dispatch.dispatch(values),result)
-        print dispatch(values)
+        print dispatch.dispatch(values)
 
     def test_100_050_RightValuesPdfEx2(self):
         values={'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         result={'altitude':'45d11.9', 'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         self.assertDictEqual(dispatch.dispatch(values),result)
-        print dispatch(values)
+        print dispatch.dispatch(values)
     # def test_100_010_ShouldBeNotNone(self):
     #     values={}
     #     self.assertDictEqual(dispatch.dispatch(values), {'error':'no op  is specified'})
