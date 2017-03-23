c96cd62fca39c4ba3f3609d97aadc338beafec16
xzw0059 <xzw0059@auburn.edu>
2017-03-22 20:57:28 -0500

    Green Light |

--- softwareprocess/wewef.py
+++ softwareprocess/wewef.py
@@ -184,10 +184,10 @@ def dispatch(values=None,dip=None):
         return values
 
 
-
+values={'observation': '10d00.0', 'height': '6.0', 'pressure': '1010', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '72'}
 # values={'observation': '10d00.0', 'height': '6.0','pressure': '1010', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '72'}
 # print dispatch(values)
         # values={'observation': '10d00.0', 'height': '6.0', 'pressure': '1010', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '72'}
-values={'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+# values={'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
 # print values['altitude']
 print dispatch(values)
