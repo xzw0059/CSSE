c466a93353cb128417c5e62e91a6d3d3b5522e4d
xzw0059 <xzw0059@auburn.edu>
2017-03-22 23:15:58 -0500

    Green Light |

--- softwareprocess/prod/convertString2DictionaryTest.py
+++ softwareprocess/prod/convertString2DictionaryTest.py
@@ -74,9 +74,9 @@ class convertString2DictionaryTest(TestCase):
         self.assertDictEqual(dispatch.dispatch(values),result)
         print values
 
-   def test_100_090_RightValuesXls2t4(self):
+   def test_100_090_RightValuesXls2t5(self):
         values={'observation': '42d0.0',  'op': 'adjust', 'extraKey':'ignore'}
-        result={'altitude':'41d59.0', 'observation': '42d0.0',  'op': 'adjust', 'extraKey':'ignore'}  
+        result={'altitude':'41d59.0', 'observation': '42d0.0',  'op': 'adjust', 'extraKey':'ignore'}
         self.assertDictEqual(dispatch.dispatch(values),result)
         print values
 
