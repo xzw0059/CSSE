735c812557067bff6603baa90b864017f1e5a016
xzw0059 <xzw0059@auburn.edu>
2017-03-22 23:46:20 -0500

    Green Light |

--- softwareprocess/prod/dispatch.py
+++ softwareprocess/prod/dispatch.py
@@ -81,10 +81,10 @@ def dispatch(values=None,dip=None):
 
 
 
-        # if (values['height'].isalpha() == ture ):
-        #     values['error'] = 'height is not invalid'
+        if (values['height'].isalpha() == True ):
+            values['error'] = 'height is not invalid'
             # __not num
-            # return values
+            return values
         if (not isinstance(float(h),float)):
             values['error'] = 'height is not invalid'
             # __not num
