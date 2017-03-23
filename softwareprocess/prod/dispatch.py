ec9faed9ab2674fb4f8c170b0afa364d753c315a
xzw0059 <xzw0059@auburn.edu>
2017-03-22 23:33:25 -0500

    Green Light |

--- softwareprocess/prod/dispatch.py
+++ softwareprocess/prod/dispatch.py
@@ -56,17 +56,21 @@ def dispatch(values=None,dip=None):
             values['error'] = 'degree is not invalid__not int'
             return values
         if  not (int(list1[0]) >= 0 and int(list1[0]) < 90):
-            values['error'] = 'degree is not invalid__not in 0~90'
+            values['error'] = 'degree is not invalid'
+            # __not in 0~90
             return values
         if not (isinstance(y,float)):
-            values['error'] = 'min is not invalid__not float'
+            values['error'] = 'min is not invalid'
+            # __not float
             return values
         if not (y >= 0 and y < 60):
-            values['error'] = 'min is not invalid__not 0~60'
+            values['error'] = 'min is not invalid'
+            # __not 0~60
             return values
 
         if (int(list1[0])==0 and y<0.1):
-            values['error'] = 'o is not invalid__o is LT 0.0.1'
+            values['error'] = 'o is not invalid'
+            # __o is LT 0.0.1
             return values
 
         if (not('height' in values)):
@@ -75,12 +79,14 @@ def dispatch(values=None,dip=None):
             h = values['height']
 
         if (not isinstance(float(h),float)):
-            values['error'] = 'height is not invalid__not num'
+            values['error'] = 'height is not invalid'
+            # __not num
             return values
 
 
         if (not float(h) >= 0):
-            values['error'] = 'height is not invalid__not >= 0'
+            values['error'] = 'height is not invalid'
+            # __not >= 0
             return values
 
         if (not('temperature' in values)):
@@ -88,10 +94,12 @@ def dispatch(values=None,dip=None):
         else:
             t =values['temperature']
         if not isinstance(float(t),float):
-            values['error'] = 'temperature is not invalid__not number'
+            values['error'] = 'temperature is not invalid'
+            # __not number
             return values
         if not (int(t) >= -20 and int(t) <= 120):
-            values['error'] = 'temperature is not invalid__not -20~120'
+            values['error'] = 'temperature is not invalid'
+            # __not -20~120
             return values
 
         tc = 5 * (float(t) - 32) / 9
@@ -104,11 +112,13 @@ def dispatch(values=None,dip=None):
 
 
         if not (isinstance(float(p),float)):
-            values['error'] = 'pressure is not invalid__not str of number'
+            values['error'] = 'pressure is not invalid'
+            # __not str of number
             return values
         if not (p >= 100 and p <= 1100):
             print  values['pressure']
-            values['error'] = 'pressure is not invalid__not 100~1100'
+            values['error'] = 'pressure is not invalid'
+            # __not 100~1100
             return values
 
         if (not('horizon' in values)):
@@ -117,10 +127,12 @@ def dispatch(values=None,dip=None):
             hr = values['horizon']
 
         if not (isinstance(hr,str)):
-            values['error'] = 'horizon is not invalid__not str'
+            values['error'] = 'horizon is not invalid'
+            # __not str
             return values
         if(not(hr == 'artificial' or 'natural')):
-            values['error'] = 'horizon is not invalid__not  artificial or natural'
+            # values['error'] = 'horizon is not invalid__not  artificial or natural'
+            values['error'] = 'horizon is not invalid'
             return values
 
         if (hr == 'natural'):
@@ -143,21 +155,26 @@ def dispatch(values=None,dip=None):
 
 
         if not (isinstance(D2,int)):
-            values['error'] = 'altitude-degree is not invalid__not int'
+            # values['error'] = 'altitude-degree is not invalid__not int'
+            values['error'] = 'altitude-degree is not invalid'
             return values
         if  not (D2 > -90 and D2 < 90):
-            values['error'] = 'altitude-degree is not invalid__not in -90~90'
+            # values['error'] = 'altitude-degree is not invalid__not in -90~90'
+            values['error'] = 'altitude-degree is not invalid'
             return values
 
         if not (isinstance(round(M2,1),float)):
-            values['error'] = 'altitude-min is not invalid__not float'
+            # values['error'] = 'altitude-min is not invalid__not float'
+            values['error'] = 'altitude-min is not invalid'
             return values
         if not (M2 >= 0 and round(M2,1) < 60):
-            values['error'] = 'altitude-min is not invalid__not 0~60'
+            # values['error'] = 'altitude-min is not invalid__not 0~60'
+            values['error'] = 'altitude is not invalid'
             return values
 
         if ('altitude' in values):
-            values['error'] = 'altitude-already exist'
+            # values['error'] = 'altitude-already exist'
+            values['error'] = 'altitude is not invalid'
             return values
 
         values['altitude'] = altitudedig
