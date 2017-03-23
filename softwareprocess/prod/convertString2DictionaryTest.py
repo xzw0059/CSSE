64ff1720407b9b60346c973eeda1f9483b7ee05e
xzw0059 <xzw0059@auburn.edu>
2017-03-23 11:23:42 -0500

    Green Light |

--- softwareprocess/prod/convertString2DictionaryTest.py
+++ softwareprocess/prod/convertString2DictionaryTest.py
@@ -103,6 +103,11 @@ class convertString2DictionaryTest(TestCase):
         result={'observation': '45d15.2', 'height': '6', 'horizon': '   ', 'pressure': '1010', 'op': 'adjust', 'temperature': '71', 'error':'horizon is invalid'}
         self.assertDictEqual(dispatch.dispatch(values),result)
 
+    def test_100_150_OnlyObsAndOp(self):
+        values={'observation': '45d15.2', 'op': 'adjust'}
+        result={'altitude': '45d14.3', 'observation': '45d15.2', 'op': 'adjust'}
+        self.assertDictEqual(dispatch.dispatch(values),result)
+
     def test_200_010_ObservationIsInvalidCauseMmnuteTooBig(self):
         values={'observation': '45d95.2', 'height': '6', 'horizon': '   ', 'pressure': '1010', 'op': 'adjust', 'temperature': '71'}
         result={'observation': '45d95.2', 'height': '6', 'horizon': '   ', 'pressure': '1010', 'op': 'adjust', 'temperature': '71','error':'observation is invalid'}
