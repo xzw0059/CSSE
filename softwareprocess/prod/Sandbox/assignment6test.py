15d507a073a78d508886c4f1b6d85c0272933646
xzw0059 <xzw0059@auburn.edu>
2017-04-22 16:12:11 -0500

    Red Light |

--- softwareprocess/prod/Sandbox/assignment6test.py
+++ softwareprocess/prod/Sandbox/assignment6test.py
@@ -183,6 +183,11 @@ class convertString2DictionaryTest(TestCase):
         values={'op':'predict', 'body': 'Betelgeuse', 'date': '2016 01 17', 'time': '03:15:42'}
         result={'op':'predict', 'body': 'Betelgeuse', 'date': '2016 01 17', 'time': '03:15:42', 'error':'invalid time'}
         self.assertDictEqual(dispatch.dispatch(values),result)
+
+    def test5_200_050_InValidValues(self):
+        values={'op':'correct', 'lat':'16d32.3', 'long':'95.41.6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3'}
+        result={'op':'predict', 'body': 'Betelgeuse', 'date': '2016 01 17', 'time': '03:15:42', 'error':'invalid time'}
+        self.assertDictEqual(dispatch.dispatch(values),result)
     # def test_100_010_ShouldBeNotNone(self):
     #     values={}
     #     self.assertDictEqual(dispatch.dispatch(values), {'error':'no op  is specified'})
