ba55a9bd4b378b7dae26e7cecaa89556aa9b0bc4
xzw0059 <xzw0059@auburn.edu>
2017-04-23 21:54:37 -0500

    Red Light |

--- softwareprocess/prod/dispatch.py
+++ softwareprocess/prod/dispatch.py
@@ -775,7 +775,9 @@ def dispatch(values=None,dip=None):
         except:
             values['error'] = 'mandatory information is missing'
             return values
-
+        print olatlist[0]
+        print olatlist[1]
+        print olatlist[2]
         try:
             (-90<olatlist[1]>90)
         except:
