1b51d62ca2430a9daaab352adb6ec812c6975719
xzw0059 <xzw0059@auburn.edu>
2017-04-24 17:00:00 -0500

    Red Light |

--- softwareprocess/prod/dispatch.py
+++ softwareprocess/prod/dispatch.py
@@ -1142,8 +1142,8 @@ def dispatch(values=None,dip=None):
 
         intermediateDistancenumbero=round(math.sin(math.radians(latnumberrad)),4)*round(math.sin(math.radians(assumedLatnumber)),4)+round(math.cos(math.radians(latnumberrad)),4)*round(math.cos(math.radians(assumedLatnumber)),4)*round(math.cos(math.radians(LHAnumber)),4)
         intermediateDistancenumber=round(intermediateDistancenumbero,3)
-
-
+        print 'print intermediateDistancenumbero='
+        print intermediateDistancenumbero
         # print intermediateDistancenumber
         correctedAltitudeas=math.asin(intermediateDistancenumbero)
         print round(correctedAltitudeas,4)
