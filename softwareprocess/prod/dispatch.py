8ff0945f1d1caff419b7e49fdacd69c7521266d7
xzw0059 <xzw0059@auburn.edu>
2017-04-24 02:24:31 -0500

    Green Light |

--- softwareprocess/prod/dispatch.py
+++ softwareprocess/prod/dispatch.py
@@ -1145,7 +1145,7 @@ def dispatch(values=None,dip=None):
 
 
         # print intermediateDistancenumber
-        correctedAltitudeas=math.asin(intermediateDistancenumber)
+        correctedAltitudeas=math.asin(intermediateDistancenumbero)
         print correctedAltitudeas
         correctedAltitudedegrees=math.degrees(correctedAltitudeas)
         print correctedAltitudedegrees
