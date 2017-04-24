68f4b857c4420bc72cd54934c02de7a5a5895631
xzw0059 <xzw0059@auburn.edu>
2017-04-24 01:54:23 -0500

    Green Light |

--- softwareprocess/prod/dispatch.py
+++ softwareprocess/prod/dispatch.py
@@ -1134,7 +1134,7 @@ def dispatch(values=None,dip=None):
             latnumberrad=float(olatlist[0])-float(olatlist[1])/60
 
 
-        if (oassumedLatlist[0]>0):
+        if (float(oassumedLatlist[0])>0):
             assumedLatnumber=float(oassumedLatlist[0])+float(oassumedLatlist[1])/60
         else:
             assumedLatnumber=float(oassumedLatlist[0])+float(oassumedLatlist[1])/60
