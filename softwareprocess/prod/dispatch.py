73d764f3d0964c61efa497874ac1342c4f2c2e84
xzw0059 <xzw0059@auburn.edu>
2017-04-24 02:26:09 -0500

    Green Light |

--- softwareprocess/prod/dispatch.py
+++ softwareprocess/prod/dispatch.py
@@ -1151,7 +1151,7 @@ def dispatch(values=None,dip=None):
         print correctedAltitudedegrees
         correctedAltitudedegreesstr=str(correctedAltitudedegrees)
         ocorrectedAltitudelist=correctedAltitudedegreesstr.split('.')
-        ocorrectedAltitudestr=str(ocorrectedAltitudelist[0]+'d'+str((ocorrectedAltitudelist[0])*60))
+        ocorrectedAltitudestr=str(ocorrectedAltitudelist[0])+'d'+str(ocorrectedAltitudelist[1])
         print ocorrectedAltitudestr
 
         #
