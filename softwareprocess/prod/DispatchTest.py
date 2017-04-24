217c2f646da917656bfdf969489a4cd39441e0d4
xzw0059 <xzw0059@auburn.edu>
2017-04-23 23:26:38 -0500

    Red Light |

--- softwareprocess/prod/DispatchTest.py
+++ softwareprocess/prod/DispatchTest.py
@@ -476,7 +476,7 @@ class MyTestCase(unittest.TestCase):
         values={'op':'correct', 'lat':'16d32.3', 'long':'', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3'}
         self.assertTrue(DP.dispatch(values).has_key("error"), True)
     def test6__200_022_InValidValueshavenotvaludoflatspace(self):
-        values={'op':'correct', 'lat':'16d32.3', 'long':' ' 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3'}
+        values={'op':'correct', 'lat':'16d32.3', 'long':' ','altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3'}
         self.assertTrue(DP.dispatch(values).has_key("error"), True)
     def test6__200_0221_InValidValueshavenotvaludoflatdots(self):
         values={'op':'correct', 'lat':'16d32.3', 'long':'.....6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3'}
