e8979a8c152f8264a492f56a54f48bc82a29c14d
xzw0059 <xzw0059@auburn.edu>
2017-04-24 01:09:38 -0500

    Green Light |

--- softwareprocess/prod/dispatch.py
+++ softwareprocess/prod/dispatch.py
@@ -1104,13 +1104,13 @@ def dispatch(values=None,dip=None):
             return values
 # ===========above is about assumedLong
 
-        longnumber=float(olonglist[0])+float(olonglist[1])/60
-        oassumedLongnumber=float(oassumedLonglist[0])+float(oassumedLonglist[1])/60
-        # print 'longnumber='
-        # print longnumber
-        #
-        # print 'oassumedLongnumber='
-        # print oassumedLongnumber
+        longnumber=float(olonglist[0])+round(float(olonglist[1])/60,2)
+        oassumedLongnumber=float(oassumedLonglist[0])+round(float(oassumedLonglist[1])/60,2)
+        print 'longnumber='
+        print longnumber
+
+        print 'oassumedLongnumber='
+        print oassumedLongnumber
 
         LHAnumber=float(olonglist[0])+float(olonglist[1])/60+float(oassumedLonglist[0])+float(oassumedLonglist[1])/60
 
