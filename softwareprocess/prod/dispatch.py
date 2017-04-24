cfe0ca431dc8e7a6896a59c0ce7113e82a73fd11
xzw0059 <xzw0059@auburn.edu>
2017-04-24 02:28:41 -0500

    Green Light |

--- softwareprocess/prod/dispatch.py
+++ softwareprocess/prod/dispatch.py
@@ -1141,7 +1141,7 @@ def dispatch(values=None,dip=None):
 
 
         intermediateDistancenumbero=round(math.sin(math.radians(latnumberrad)),4)*round(math.sin(math.radians(assumedLatnumber)),4)+round(math.cos(math.radians(latnumberrad)),4)*round(math.cos(math.radians(assumedLatnumber)),4)*round(math.cos(math.radians(LHAnumber)),4)
-        intermediateDistancenumber=round(intermediateDistancenumbero,4)
+        intermediateDistancenumber=round(intermediateDistancenumbero,2)
 
 
         # print intermediateDistancenumber
