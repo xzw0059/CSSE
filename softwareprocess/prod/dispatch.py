1e3551f3e2cc368a8720b3945dbb2c89c98496f6
xzw0059 <xzw0059@auburn.edu>
2017-04-24 19:36:30 -0500

    Red Light |

--- softwareprocess/prod/dispatch.py
+++ softwareprocess/prod/dispatch.py
@@ -1189,12 +1189,12 @@ def dispatch(values=None,dip=None):
         print ('cosasinintermediateDistance = %s'%cosasinintermediateDistance)
 
         correctedAzimuthround1=round(correctedAzimuth1/(cosassumedLat*cosasinintermediateDistance),3)
-    
+
         correctedAzimuthnumber=math.acos(correctedAzimuth1/(cosassumedLat*cosasinintermediateDistance))
         correctedAzimuth=round(correctedAzimuthnumber,999)
 
 
-        print ('correctedAzimuth1/(cosassumedLat*cosasinintermediateDistance) = %s'%(correctedAzimuth1/(cosassumedLat*cosasinintermediateDistance)))
+        print ('correctedAzimuthround1= %s'%correctedAzimuthround1)
 
         print ('correctedAzimuth=%s'%correctedAzimuth)
 
