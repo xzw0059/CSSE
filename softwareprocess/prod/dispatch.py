ca4b78db3569a5f8ab6b8b06a348be6b0ac8d062
xzw0059 <xzw0059@auburn.edu>
2017-04-24 19:33:11 -0500

    Red Light |

--- softwareprocess/prod/dispatch.py
+++ softwareprocess/prod/dispatch.py
@@ -1173,8 +1173,8 @@ def dispatch(values=None,dip=None):
 # should be:
 #         correctedAzimuth  = arccos((sin(lat) - (sin(assumedLat) * intermediateDistance))/(cos(assumedLat) * cos(arcsin(intermediateDistance))))
         sinlat=round(math.sin(math.radians(latnumberrad)),4)
-        sinassumedLat=round(math.sin(math.radians(assumedLatnumber)),3)
-        intermediateDistancenumberoround=round(intermediateDistancenumbero,3)
+        sinassumedLat=round(math.sin(math.radians(assumedLatnumber)),4)
+        intermediateDistancenumberoround=round(intermediateDistancenumbero,4)
         correctedAzimuth1=(sinlat-sinassumedLat*intermediateDistancenumberoround)
         cosassumedLat=round(math.cos(math.radians(assumedLatnumber)),4)
         asinintermediateDistance=math.asin(intermediateDistancenumbero)
@@ -1188,7 +1188,8 @@ def dispatch(values=None,dip=None):
         print ('cosassumedLat = %s'%cosassumedLat)
         print ('cosasinintermediateDistance = %s'%cosasinintermediateDistance)
 
-        correctedAzimuth=math.acos(correctedAzimuth1/(cosassumedLat*cosasinintermediateDistance))
+        correctedAzimuthnumber=math.acos(correctedAzimuth1/(cosassumedLat*cosasinintermediateDistance))
+        correctedAzimuth=round(correctedAzimuthnumber)
 
 
         print ('correctedAzimuth1/(cosassumedLat*cosasinintermediateDistance) = %s'%(correctedAzimuth1/(cosassumedLat*cosasinintermediateDistance)))
