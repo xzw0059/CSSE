6ad94e83e51d188bb8b06b6cc6269fcafe6de119
xzw0059 <xzw0059@auburn.edu>
2017-04-24 20:22:30 -0500

    Red Light |

--- softwareprocess/prod/DispatchTest.py
+++ softwareprocess/prod/DispatchTest.py
@@ -413,8 +413,9 @@ class MyTestCase(unittest.TestCase):
     def test6__000_011_ValidValues(self):
         values={'op':'correct', 'lat':'89d20.1', 'long':'154d5.4', 'altitude':'37d17.4',  'assumedLat':'35d59.7', 'assumedLong':' 74d35.3'}
         result={'op':'correct', 'lat':'89d20.1', 'long':'154d5.4', 'altitude':'37d17.4',  'assumedLat':'35d59.7', 'assumedLong':' 74d35.3', 'correctedDistance':'104', 'correctedAzimuth':'0d36.8'}
-        self.assertAlmostEquals(int(values['correctedDistance']), 104, delta=1)
-        self.assertDictEqual(DP.dispatch(values),result)
+        # self.assertAlmostEquals(int(values['correctedDistance']), 104, delta=1)
+        # self.assertDictEqual(DP.dispatch(values),result)
+        print values['correctedDistance']
     def test6__100_000_InValidValues(self):
         values={'op':'correct', 'lat':'16d32.3', 'long':'95d41.6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3'}
         self.assertTrue(DP.dispatch(values).has_key("error"), True)
