8eef4fd8e48c2244f7417ce8cc564d2efb0714ac
xzw0059 <xzw0059@auburn.edu>
2017-04-26 16:32:05 -0500

    Red Light |

--- softwareprocess/prod/dispatch.py
+++ softwareprocess/prod/dispatch.py
@@ -1129,6 +1129,7 @@ def dispatch(values=None,dip=None):
 
         longnumber=float(olonglist[0])+float(olonglist[1])/60.0
         oassumedLongnumber=float(oassumedLonglist[0])+float(oassumedLonglist[1])/60
+        print float(oassumedLonglist[0])
         print float(oassumedLonglist[1])/60
         print type(float(oassumedLonglist[1])/60)
         print ('float(oassumedLonglist[1])/60.0=%d'%(float(oassumedLonglist[1])/60))
