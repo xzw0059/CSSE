e1ec9bcee6dc649b9822b82ecfef3a35cfe6d9e6
xzw0059 <xzw0059@auburn.edu>
2017-04-26 16:13:12 -0500

    Red Light |

--- softwareprocess/prod/DispatchTest.py
+++ softwareprocess/prod/DispatchTest.py
@@ -43,13 +43,14 @@ class MyTestCase(unittest.TestCase):
         values={'op':'correct', 'lat':'16D32.3', 'long':'95d41.6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3'}
         result={'op':'correct', 'lat':'16D32.3', 'long':'95d41.6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3', 'correctedDistance':'3950', 'correctedAzimuth':'164d42.9'}
         self.assertDictEqual(DP.dispatch(values),result)
-    def test6__000_015_ValidValues(self):
-        values={'op':'correct', 'lat':'16Dd32.3', 'long':'95d41.6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3'}
-        result={'op':'correct', 'lat':'16Dd32.3', 'long':'95d41.6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3', 'correctedDistance':'3950', 'correctedAzimuth':'164d42.9'}
-        self.assertDictEqual(DP.dispatch(values),result)
+
     def test6__200_010_inValidValues(self):
         values={'op':'correct', 'lat':'16aad32.3', 'long':'95d41.6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3'}
         result={'op':'correct', 'lat':'16d32.3', 'long':'95d41.6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3', 'correctedDistance':'3950', 'correctedAzimuth':'164d42.9'}
+        self.assertTrue(DP.dispatch(values).has_key("error"), True)
+    def test6__200_011_ValidValues(self):
+        values={'op':'correct', 'lat':'16Dd32.3', 'long':'95d41.6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3'}
+        result={'op':'correct', 'lat':'16Dd32.3', 'long':'95d41.6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3', 'correctedDistance':'3950', 'correctedAzimuth':'164d42.9'}
         self.assertTrue(DP.dispatch(values).has_key("error"), True)
     def test6__200_020_InValidValueshavenotlat(self):
         values={'op':'correct','long':'95.41.6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3'}
